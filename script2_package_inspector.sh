51c63caa18c6d0e830ca157c0e7f997055b9314a
bhavin-01 <bhavinwork1@gmail.com>
2026-03-29 22:31:29 +0530

    Update script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
+# -----------------------------------------------------
 # Script 2: FOSS Package Inspector
+# Author: Bhavin
+# Purpose:
+#   Checks if a specific package is installed on the system.
+#   Displays version and license information when available.
+#   Uses if-then-else and case statements.
+# -----------------------------------------------------
 
-PACKAGE="python3"
+PACKAGE_NAME="python3"
 
-echo "Checking package: $PACKAGE"
-echo "-------------------------"
+echo "Checking installation status for: $PACKAGE_NAME"
+echo "-----------------------------------------------"
 
-if dpkg -l | grep -q $PACKAGE; then
-    echo "$PACKAGE is installed."
-    dpkg -s $PACKAGE | grep -E 'Version|Maintainer|Description'
+# Check using dpkg (Debian/Ubuntu systems)
+if dpkg -l | grep -qw $PACKAGE_NAME; then
+    echo "$PACKAGE_NAME is installed on this system."
+    echo ""
+
+    echo "Package Information:"
+    dpkg -s $PACKAGE_NAME | grep -E 'Version|Maintainer|Description'
+
 else
-    echo "$PACKAGE is NOT installed."
+    echo "$PACKAGE_NAME is NOT installed."
 fi
 
 echo ""
 
-case $PACKAGE in
+# Provide a philosophical note about well-known FOSS tools
+case $PACKAGE_NAME in
     python3)
-        echo "Python: A community-driven programming language used worldwide"
+        echo "Python: A powerful community-driven programming language."
         ;;
     apache2)
-        echo "Apache: One of the most popular open source web servers"
+        echo "Apache: The web server that helped build the open internet."
         ;;
     mysql-server)
-        echo "MySQL: Database powering millions of applications"
+        echo "MySQL: Open-source database powering many modern applications."
         ;;
     vlc)
-        echo "VLC: Open source media player capable of playing any format"
+        echo "VLC: A universal media player built by an open community."
         ;;
     *)
-        echo "Unknown package"
+        echo "This package is not in the predefined philosophy list."
         ;;
 esac
+
+echo ""
+echo "Inspection complete."
